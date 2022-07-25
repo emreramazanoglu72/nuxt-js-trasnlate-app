d0f906fed626f358f7e42e943e4a10507042a9bc
Emre Ramazanoğlu <73821776+emreramazanoglu72@users.noreply.github.com>
2022-07-25 16:59:34 +0300

    fixxa
    fixxa

--- modules/TrasnlateRequest.js
+++ modules/TrasnlateRequest.js
@@ -1,7 +1,7 @@
 
 const fetch = require("node-fetch");
 
-const TrasnlateRequest = async (text,tt,to) => {
+const TrasnlateRequest = async (text = "",tt = "",to = "") => {
     const request = await fetch(`https://api.emreramazanoglu.com.tr/translate/?text=${text}&tt=${tt}&to=${to}`);
     const response = await request.text();
     return response
