0af1340de74ac4cf35a2ba8aef1e965cd434b0b5
Emre Ramazanoğlu <73821776+emreramazanoglu72@users.noreply.github.com>
2022-07-25 16:53:17 +0300

    Update TrasnlateRequest.js

--- modules/TrasnlateRequest.js
+++ modules/TrasnlateRequest.js
@@ -2,7 +2,7 @@
 const fetch = require("node-fetch");
 
 const TrasnlateRequest = async (text,tt,to) => {
-    const request = await fetch(`https://api.priv8area.com/translate/?text=${text}&tt=${tt}&to=${to}`);
+    const request = await fetch(`https://api.emreramazanoglu.com.tr/translate/?text=${text}&tt=${tt}&to=${to}`);
     const response = await request.text();
     return response
 }
